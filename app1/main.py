b97d865d8d9fc2dfb0d1b3657aa5bc3b88a18882
sandokhan <rhaydrick@gmail.com>
2023-02-08 21:01:00 +0000

    Complete day 4.

--- app1/main.py
+++ app1/main.py
@@ -3,6 +3,20 @@ user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip().lower()
+
+    match user_action:
+        case 'add': 
+            todo = input(user_prompt)
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item.title())
+        case 'exit':
+            break
+        case _:
+            print("Hey, you entered a unkown command.")
+
+print("Bye, Bye!")
+
