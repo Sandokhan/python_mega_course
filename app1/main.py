87dc5381cbc80d3baf7b34ddf32eebb6e5e2ef4c
sandokhan <rhaydrick@gmail.com>
2023-02-09 08:02:05 +0000

    Add complete to code, and completed day 5.

--- app1/main.py
+++ app1/main.py
@@ -3,7 +3,7 @@ user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
 
     match user_action:
@@ -11,13 +11,17 @@ while True:
             todo = input(user_prompt)
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item.title())
+            for index, item in enumerate(todos):
+                row = f"{index + 1}. {item.title()}"
+                print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number =- 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+             number = int(input("Number of the todo to complete: "))
+             todos.pop(number-1)
         case 'exit':
             break
         case _:
