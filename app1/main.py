18e01b6fdadeb57499bb65cecb498a90d28e56dc
sandokhan <rhaydrick@gmail.com>
2023-02-11 20:17:43 +0000

    Add docstring to functions.

--- app1/main.py
+++ app1/main.py
@@ -1,12 +1,16 @@
 user_prompt = "Enter a todo: "
 
 def get_todos(filename):
+    """ Read a text file and return the list of
+     to-do items.
+    """
     with open(f'./files/{filename}', 'r') as file:
         todos = file.readlines()
     return todos
 
 
 def write_todos(filename, todos_args):
+    """ Write the to-do item list in the file. """
     with open(f'./files/{filename}', 'w') as file:
         file.writelines(todos_args)
     return None
