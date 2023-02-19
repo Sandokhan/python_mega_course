6ac064d6a490e9be80382bc02bad271658f2c74c
sandokhan <rhaydrick@gmail.com>
2023-02-19 09:21:06 +0000

    Completed day 17.

--- app1/gui.py
+++ app1/gui.py
@@ -9,8 +9,10 @@ list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button('Edit')
 
+layout = [[label], [input_box, add_button], [list_box,edit_button]]
+
 window = sg.Window('My To-Do App', 
-                    layout=[[label], [input_box, add_button], [list_box,edit_button]], 
+                    layout=layout, 
                     font=('Helvetica', 20))
 
 while True:
