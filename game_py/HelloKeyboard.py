2c2f80a9e1a4f7ffa0d10dff154f9600419e2557
sandokhan <rhaydrick@gmail.com>
2023-02-19 21:06:58 +0000

    Add more controler to sprite.

--- game_py/HelloKeyboard.py
+++ game_py/HelloKeyboard.py
@@ -11,27 +11,30 @@ spriteWidth = sprite1.get_width()
 spriteHeight = sprite1.get_height()
 
 pygame.display.set_caption("Hello Keyboard")
-screen.fill((0,0,0))
 
-#game_over = False
 x, y = (0,0)
+game_over = False
+clock = pygame.time.Clock()
+while not game_over:
 
-while True:
+    dt = clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            #game_over = True
-            break
+            game_over = True
     pressed = pygame.key.get_pressed()
 
     if pressed[K_UP]:
-        y-= .5
+        y-= .5 * dt
     if pressed[K_DOWN]:
-        y+= .5
+        y+= .5 * dt
     if pressed[K_LEFT]:
-        x-= .5
+        x-= .5 * dt
     if pressed[K_RIGHT]:
-        x+= .5
+        x+= .5 * dt
+    if pressed[K_SPACE]:
+        x, y = (0, 0)
 
+    screen.fill((0,0,0))
     screen.blit(sprite1, (x, y))
     pygame.display.update()
 pygame.quit()
