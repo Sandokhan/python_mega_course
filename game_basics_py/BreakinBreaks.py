550f630143bfe9c3e4d6a468985b912a3f2c09ab
sandokhan <rhaydrick@gmail.com>
2023-02-20 20:17:45 +0000

    Adding basic moviments to ball.

--- game_basics_py/BreakinBreaks.py
+++ game_basics_py/BreakinBreaks.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 
@@ -14,6 +15,13 @@ bat_rect = bat.get_rect()
 ball = pygame.image.load('./images/football.png')
 ball = ball.convert_alpha()
 ball_rect = ball.get_rect()
+ball_start = (200, 200)
+ball_speed = (3.0, 3.0)
+ball_served = False
+speedx, speedy = ball_speed
+ball_rect.topleft = ball_start
+
+bat_rect[1] = screen.get_height() - 100
 
 #brick
 brick = pygame.image.load('./images/brick.png')
@@ -33,18 +41,55 @@ for y in range(brick_rows):
 
 clock = pygame.time.Clock()
 game_over = False
-
+x,y = (0, 0)
 while not game_over:
 
     dt = clock.tick(50)
     screen.fill((0, 0, 0))
 
+    # Adding Bricks
     for b in bricks:
         screen.blit(brick, b)
+
+    # Adding Paddle
+    screen.blit(bat, bat_rect)
+
+    # Adding Ball
+    screen.blit(ball, ball_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    pressed = pygame.key.get_pressed()
+
+    if pressed[K_LEFT]:
+        x -= .5 * dt
+    if pressed[K_RIGHT]:
+        x += .5 * dt
+    if pressed[K_SPACE]:
+        ball_served = True
+
+    # Top
+    if ball_rect[1] <= 0:
+        ball_rect[1] = 0
+        speedy *= -1
+    # Bottom
+    if ball_rect[1] >= screen.get_height() - ball_rect.height:
+        ball_rect[1] = screen.get_height() - ball_rect.height
+        speedy *= -1
+    # Left
+    if ball_rect[0] <= 0:
+        ball_rect[0] = 0
+        speedx *= -1
+    # Right
+    if ball_rect[0] >= screen.get_width() - ball_rect.width:
+        ball_rect[0] = screen.get_width() - ball_rect.width
+        speedx *= -1
+
+    bat_rect[0] = x
+    if ball_served:
+        ball_rect[0] += speedx
+        ball_rect[1] += speedy
     pygame.display.update()
 pygame.quit()
