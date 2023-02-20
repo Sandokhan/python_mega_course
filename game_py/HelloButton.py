e3c92fb97d547013bc923ff8053e16ce380961d1
sandokhan <rhaydrick@gmail.com>
2023-02-20 05:18:54 +0000

    Add basic implementation of a button to close display.

--- game_py/HelloButton.py
+++ game_py/HelloButton.py
@@ -16,14 +16,31 @@ button_rect = [screen.get_width()/2 - button_width/2,
                screen.get_height()/2 - button_height/2,
                button_width, button_height]
 
-game_over = False
+button_font = pygame.font.SysFont('Arial', 20)
+button_text = button_font.render('Quit', True, text_coulor)
+screen.fill((100, 100, 100))
 
+game_over = False
+x, y = (0, 0)
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if (button_rect[0] <= x <= button_rect[0] + button_rect[2] and
+                    button_rect[1] <= y <= button_rect[1] + button_rect[3]):
+                game_over = True
+        if event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
 
-    pygame.draw.rect(screen, button_coulor, button_rect)
+    if (button_rect[0] <= x <= button_rect[0] + button_rect[2] and
+            button_rect[1] <= y <= button_rect[1] + button_rect[3]):
+        pygame.draw.rect(screen, button_over_coulor, button_rect)
+    else:
+        pygame.draw.rect(screen, button_coulor, button_rect)
+    screen.blit(button_text, (button_rect[0] + (button_width - button_text.get_width())/2,
+                              button_rect[1] + (button_height - button_text.get_height())/2))
     pygame.display.update()
 
 pygame.quit()
