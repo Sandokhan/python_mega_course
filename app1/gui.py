7070b385cae4b9e7ed6d09c680cb5812f2576de9
sandokhan <rhaydrick@gmail.com>
2023-02-20 21:20:20 +0000

    Change Add and complete buttons.

--- app1/gui.py
+++ app1/gui.py
@@ -6,11 +6,13 @@ sg.theme('Black')
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter todo', key="todo")
-add_button = sg.Button("Add", size=15)
+add_button = sg.Button(size=2, image_source='./images/add.png', mouseover_colors='LightBlue2',
+                       tooltip="Add todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', 
                       enable_events=True, size=[45,10])
 edit_button = sg.Button('Edit')
-complete_button = sg.Button('Complete')
+complete_button = sg.Button(size=2, image_source='./images/complete.png', mouseover_colors='LightBlue2',
+                            tooltip="Add todo",key='Complete')
 exit_button = sg.Button('Exit')
 
 layout = [[clock],
